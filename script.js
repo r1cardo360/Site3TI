264945a667af98255075741d7095b703f5ccf0ea
Ricardo <ric.furlanetto@hotmail.com>
2024-08-19 16:46:17 -0300

    Adicionando mais coisas aos arquivos

---
+++ script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('.my-carousel').slick({
+        dots: true, // Adiciona pontos de navegação
+        infinite: false, // Permite loop infinito
+        speed: 0, // Velocidade de transição
+        slidesToShow: 3, // Quantos slides mostrar por vez
+        slidesToScroll: 1 // Quantos slides rolar por vez
+    });
+});
