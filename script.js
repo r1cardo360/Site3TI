b7615d89e53c3b30d92a6d1df74aeec92f6a7eff
Ricardo <ric.furlanetto@hotmail.com>
2024-08-23 16:00:46 -0300

    Terminando o carrosel de segmentos

--- script.js
+++ script.js
@@ -31,3 +31,38 @@ $('.slick-carousel-produtos').slick({
       }
     ]
   });
+
+
+  $('.slick-carousel-segmentos').slick({
+    dots: true,
+    infinite: true,
+    speed: 600,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows:false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
